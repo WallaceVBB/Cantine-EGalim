2a697e2956d0b2d820616c89fde6e0873c59cc2e
WallaceVBB <wallacevictor@etik.com>
2026-09-09 17:20:07 +0200

    New release v0.1.4

--- utils.py
+++ utils.py
@@ -16,7 +16,7 @@ console = Console() # console pour enrichir les impressions dans le terminal (co
 
 # Variable d'environnement permettant de forcer le répertoire de données (utile pour les tests)
 NOM_APPLICATION = "EpiData"
-VERSION = "0.1.3"
+VERSION = "0.1.4"
 VARIABLE_ENV_USER_DIR = "EPIDATA_USER_DIR"
 
 def _est_empaquete():
